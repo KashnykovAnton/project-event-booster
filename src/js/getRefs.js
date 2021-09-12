acabc7fa5c8802698536bca9e77084a802abbb51
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-09-12 10:41:29 +0200

    createt a folder js

---
+++ src/js/getRefs.js
@@ -0,0 +1,8 @@
+function getRefs() {
+  return {
+    formRef: document.querySelector(''), //ссылка на будуйщую форму
+  };
+}
+
+export { getRefs } from './searchEvent';
+export { getRefs } from '../index';
---
+++ src/js/searchEvent.js
@@ -0,0 +1,16 @@
+import { getRefs } from './getRefs';
+import { fetchEvent } from './apiService';
+
+const refs = getRefs();
+
+function onSearchEvent(e) {
+  e.preventDefault();
+  refs.formRef.query = e.currentTarget.elements.query.value;
+  const searchEvent = e.currentTarget.elements.query.value;
+
+  fetchEvent(searchEvent)
+    .then(events => console.log(events)) // успешно получаем 20 events
+    .catch();
+}
+
+refs.formRef.addEventListener('submit', onSearchEvent);
