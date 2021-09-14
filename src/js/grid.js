78d9efd47cb5d8b7f25c2295997bbfa13b4b07c9
OksiDavydova <oksana.acva@gmail.com>
2021-09-15 00:12:42 +0300

    hero title + header

--- src/js/grid.js
+++ src/js/grid.js
@@ -1,17 +1,20 @@
-import fetchEvent from '../js/apiService'
+import fetchEvent from "../js/apiService";
 
-const mainListRef = document.querySelector('.main-list')
+const mainListRef = document.querySelector(".main-list");
 
 function createMarkup(data) {
-    console.log(data.events[0].images)
-    data.events.map(el => { 
-        mainListRef.insertAdjacentHTML('beforeend', `<li class="main-item">
+  console.log(data.events[0].images);
+  data.events.map((el) => {
+    mainListRef.insertAdjacentHTML(
+      "beforeend",
+      `<li class="main-item">
         <img class="main-poster" src="${el.images[0].url}" alt="">
         <h2 class="main-name">${el.name}</h2>
         <p class="main-date">${el.dates.start.localDate}</p>
         <p class="main-location">${el.dates.timezone}</p>
-        </li>`)
-     })
+        </li>`
+    );
+  });
 }
 
-fetchEvent().then(createMarkup)
+fetchEvent().then(createMarkup);
