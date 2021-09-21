f790be3d5c2e6f9e6e532d425b6d0184f9cf7cde
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-09-21 16:15:29 +0200

    mini edit

--- src/js/scrollPagePagination.js
+++ src/js/scrollPagePagination.js
@@ -2,7 +2,7 @@ import { refs } from './getRefs';
 
 var btn = $('.pagination');
 
-btn.on('click', function (e) {
+btn.on('click', function () {
   $('html, body').animate({ scrollTop: 200 }, 1300);
 });
 
