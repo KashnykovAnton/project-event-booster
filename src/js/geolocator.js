a9dbb8eb4741b25e019ebf59bd5571afb7debdde
KashnykovAnton <kashnikov_anton@yahoo.com>
2022-05-19 23:12:58 +0200

    try commit

--- src/js/geolocator.js
+++ src/js/geolocator.js
@@ -5,6 +5,7 @@ import { refs } from './getRefs';
 import { hideLoader } from './loader';
 
 const GEO_KEY = 'AntonMail';
+const API_KEY = '0cc70647f413003b02de263750cf9a3e';
 
 window.onload = geolocate();
 
@@ -15,14 +16,24 @@ function geolocate() {
 function onGetPositionError() {
   refs.mainListRef.innerHTML =
     '<div class="error_Geo animate__animated animate__zoomInDown"><h2>Sorry, but we could not access your geolocation :(</h2> <h3>Please select your country :)</h3></div>';
+  hideLoader();
 }
 
 async function onGetPositionSuccess(position) {
-  const response = await fetch(
-    `http://api.geonames.org/countryCodeJSON?lat=${position.coords.latitude}&lng=${position.coords.longitude}&username=${GEO_KEY}`,
-  );
-  const data = await response.json();
-  return fetchEventWithGeolocation(data.countryCode);
+  try {
+    const response = await fetch(
+      // `https://api.ipapi.com/api/check?access_key=${API_KEY}`,
+      `https://api.ipapi.com/api/check?access_key=${API_KEY}`,
+      // `http://api.geonames.org/countryCodeJSON?lat=${position.coords.latitude}&lng=${position.coords.longitude}&username=${GEO_KEY}`,
+    );
+    const data = await response.json();
+    console.log(data);
+    // return fetchEventWithGeolocation(data.countryCode);
+    return fetchEventWithGeolocation(data.country_code);
+  } catch (error) {
+    console.log(error);
+    onGetPositionError();
+  }
 }
 
 function fetchEventWithGeolocation(country) {
